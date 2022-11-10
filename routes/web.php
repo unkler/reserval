73c2231813ff851925b61f6d775578ebde567c71
unkler <unkler0830@gmail.com>
2022-11-11 00:32:24 +0900

    Gate追加

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('manager')->middleware('can:manager-higher')->group(function () {
+    Route::get('index', function () {
+        dd('manager');
+    });
+});
+
+Route::middleware('can:user-higher')->group(function () {
+    Route::get('index', function () {
+        dd('user');
+    });
+});
+
 Route::controller(LiveWireTestController::class)
-    ->prefix('livewire-test')->name('livewire-test.')->group(function() {
+    ->prefix('livewire-test')->name('livewire-test.')->group(function () {
         Route::get('index', 'index')->name('index');
         Route::get('register', 'register')->name('register');
-});
+    });
 
 Route::get('alpine-test/index', [AlpineTestController::class, 'index']);
 
