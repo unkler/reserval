13d0fd30a9e199bb27b2b07561a4fc6f55cc6f66
unkler <unkler0830@gmail.com>
2022-11-12 20:22:06 +0900

    event dummy修正

--- resources/js/flatpicker.js
+++ resources/js/flatpicker.js
@@ -8,6 +8,13 @@ flatpickr("#event_date", {
   maxDate: new Date().fp_incr(30)
 });
 
+flatpickr("#calendar", {
+  "locale": Japanese,
+  // minDate: "today",
+  maxDate: new Date().fp_incr(30)
+});
+
+
 const timeSetting = {
   "locale": Japanese,
   enableTime: true,
@@ -16,6 +23,7 @@ const timeSetting = {
   time_24hr: true,
   minTime: "10:00",
   maxTime: "20:00",
+  minuteIncrement: 30,
 }
 
 flatpickr("#start_time", timeSetting);
